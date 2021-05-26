2709b0da1b80508871edf661d7331fca588f9fc3
mahmutsvlms <77540261+mahmutsvlms@users.noreply.github.com>
2021-05-26 23:40:02 +0300

    Updated Repository D.44

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concreate;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında Kısmını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterSurName).MinimumLength(3).WithMessage("Lütfen En Az 2 Karakter Girişi Yapın");
+            RuleFor(x => x.WriterSurName).MaximumLength(50).WithMessage("Lütfen 50 Karakterden Fazla Değer Girişi Yapmayın");
+        }
+    }
+}
