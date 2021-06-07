053d0b8123cabb22cf62184bf34ead029df3adcb
mahmutsvlms <77540261+mahmutsvlms@users.noreply.github.com>
2021-06-07 23:55:55 +0300

    Updated Repository D.60

--- MvcProjeKampi/Controllers/ContactController.cs
+++ MvcProjeKampi/Controllers/ContactController.cs
@@ -19,5 +19,10 @@ namespace MvcProjeKampi.Controllers
             var contactValues = cm.GetList();
             return View(contactValues);
         }
+        public ActionResult GetContactDetails(int id)
+        {
+            var contactValues = cm.GetByID(id);
+            return View(contactValues);
+        }
     }
 }
